3ebe8ae80f2e1012598b0a4a241c3e133b97d8cc
sidgharge <ghargesiddharth@gmail.com>
2022-03-18 21:05:06 +0530

    added main method and README

---
+++ README.md
@@ -0,0 +1,8 @@
+# CommandLineCalculator
+Accepts an expression from console and evaluates it.
+For example 5 * (2 + 1) will give output of 15
+
+## How to run
+To run it import the project(this is a maven project) in you favourite IDE.
+Run the main method in com.homeprojects.commandlinecalculator.CommandLineCalculator class.
+Project also has test cases in test directory which you can run.
--- src/main/java/com/homeprojects/commandlinecalculator/CommandLineCalculator.java
+++ src/main/java/com/homeprojects/commandlinecalculator/CommandLineCalculator.java
@@ -2,9 +2,24 @@ package com.homeprojects.commandlinecalculator;
 
 import com.homeprojects.commandlinecalculator.parser.Parser;
 
+import java.util.Scanner;
+
 public class CommandLineCalculator {
 
     public int evaluate(String expression) {
         return new Parser(expression).parse().evaluate();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            String expression = scanner.nextLine();
+            int result = new CommandLineCalculator().evaluate(expression);
+            System.out.println(result);
+        } catch (Exception e) {
+            e.getMessage();
+            e.printStackTrace();
+        }
+        scanner.close();
     }
 }
